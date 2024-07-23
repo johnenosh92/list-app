372ae02dc6e7e131dc98ac985aa326496c6bbeda
johnenosh92 <johnenosh92@gmail.com>
2024-07-23 10:10:32 +0530

    Final commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TasksComponent } from './taskList/tasks/tasks.component';
+import { AddEditComponent } from './add-edit/add-edit.component';
+import { TasksService } from './taskList/services/tasks.service';
+import { AppRoutingModule } from './app-routing.module'; // Import your routing module
+import { ConfirmationDialogComponent } from './taskList/confirm-dialogue/confirmation-dialog.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { TasksComponent } from './taskList/tasks/tasks.component';
-
 import {MatButtonModule} from '@angular/material/button';
-
 import {MatTableModule} from '@angular/material/table';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatIconModule} from '@angular/material/icon';
-import { AddEditComponent } from './add-edit/add-edit.component';
-import { TasksService } from './taskList/services/tasks.service';
-import { AppRoutingModule } from './app-routing.module'; // Import your routing module
-
-import { ConfirmationDialogComponent } from './taskList/confirm-dialogue/confirmation-dialog.component';
 
 @NgModule({
   imports: [
